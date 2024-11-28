a873611993885ebbd37b42e9c96f99a4df687768
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-11-28 21:27:41 +0530

    Added shadecn Support

--- ByteBoulevard/src/App.jsx
+++ ByteBoulevard/src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BannerContainer from './components/Banner/BannerContainer'
-
 function App() {
   return (
     <>
